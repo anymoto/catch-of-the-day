fd96eaefe098fbe67e3e4ce8c20fab79ac4882be
anymoto <ana@castromartinez.com>
2016-01-01 23:22:35 -0600

    Add form submit event listener

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,7 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var Navigation = ReactRouter.Navigation;
+var History = ReactRouter.History;
 
 var createBrowserHistory = require('history/lib/createBrowserHistory');
 
@@ -77,11 +78,16 @@ var Inventory = React.createClass({
 */
 
 var StorePicker = React.createClass({
+  mixins: [History],
+  goToStore: function(event) {
+    event.preventDefault();
+    // get the data from the input
+    var storeId = this.refs.storeId.value;
+    this.history.pushState(null, '/store/' + storeId)
+  },
   render: function() {
-  var name = 'Ana';
-
     return (
-      <form className='store-selector'>
+      <form className='store-selector' onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
         <input type='text' ref='storeId' defaultValue={h.getFunName()}
           required />
