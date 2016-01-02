258ade87ab5b1879ad16d0f052de538d09f2cd42
anymoto <ana@castromartinez.com>
2016-01-01 22:09:40 -0600

    Routing with React Router

---
+++ scripts/main.js
@@ -0,0 +1,97 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactRouter = require('react-router');
+
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Navigation = ReactRouter.Navigation;
+
+/*
+  App
+*/
+var App = React.createClass({
+  render: function() {
+    return (
+      <div className='catch-of-the-day'>
+        <div className='menu'>
+          <Header tagline='Fresh Seafood Good' />
+        </div>
+        <Order />
+        <Inventory />
+      </div>
+    )
+  }
+});
+
+/*
+  Header
+*/
+
+var Header = React.createClass({
+  render: function() {
+    return (
+      <header className='top'>
+        <h1>Catch
+          <span className='ofThe'>
+            <span className='of'>of</span>
+            <span className='the'>the</span>
+          </span>
+        Day</h1>
+        <h3 className='tagline'>{ this.props.tagline }</h3>
+      </header>
+    )
+  }
+});
+
+/*
+  Order
+*/
+
+var Order = React.createClass({
+  render: function() {
+    return (
+      <p>Order</p>
+    )
+  }
+});
+
+/*
+  Inventory
+*/
+
+var Inventory = React.createClass({
+  render: function() {
+    return (
+      <p>Inventory</p>
+    )
+  }
+});
+
+/*
+  StorePicker
+  This will let us make <StorePicker/>
+*/
+
+var StorePicker = React.createClass({
+  render: function() {
+  var name = 'Ana';
+
+    return (
+      <form className='store-selector'>
+        <h2>Please Enter A Store</h2>
+        <input type='text' ref='storeId' required />
+        <input type='Submit' />
+      </form>
+    )
+  }
+});
+
+
+var routes = (
+  <Router>
+    <Route path="/" component={StorePicker} />
+    <Route path="/store/:storeId" component={App} />
+  </Router>
+)
+
+ReactDOM.render(routes, document.querySelector('#main'));
