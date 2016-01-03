9b851823d7bbd1230c101c627c02d5a8a7703ef4
anymoto <ana@castromartinez.com>
2016-01-03 16:45:54 -0600

    Autobinding this with ES7 decorators

--- scripts/components/StorePicker.js
+++ scripts/components/StorePicker.js
@@ -4,18 +4,22 @@
 */
 
 import React from 'react';
-import {History} from 'react-router';
-import h from '../helpers'
+import { History } from 'react-router';
+import h from '../helpers';
+import reactMixin from 'react-mixin';
+import autobind from 'autobind-decorator';
 
-var StorePicker = React.createClass({
-  mixins: [History],
-  goToStore: function(event) {
+@autobind
+class StorePicker extends React.Component {
+
+  goToStore(event) {
     event.preventDefault();
     // get the data from the input
     var storeId = this.refs.storeId.value;
-    this.history.pushState(null, '/store/' + storeId)
-  },
-  render: function() {
+    this.history.pushState(null, '/store/' + storeId);
+  }
+
+  render() {
     return (
       <form className='store-selector' onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
@@ -25,6 +29,8 @@ var StorePicker = React.createClass({
       </form>
     )
   }
-});
+}
+
+reactMixin.onClass(StorePicker, History);
 
 export default StorePicker;
