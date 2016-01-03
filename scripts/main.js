4bedd5c5580af0c3cd1da03cdcbceb26876e47e9
anymoto <ana@castromartinez.com>
2016-01-02 20:40:52 -0600

    Persisting state with LocalStorage

--- scripts/main.js
+++ scripts/main.js
@@ -12,6 +12,12 @@ var createBrowserHistory = require('history/lib/createBrowserHistory');
 var h = require('./helpers');
 
 /*
+  Firebase
+*/
+var Rebase = require('re-base');
+var base = Rebase.createClass('https://catch-of-the-day-ana.firebaseio.com/');
+
+/*
   App
 */
 var App = React.createClass({
@@ -20,6 +26,26 @@ var App = React.createClass({
       fishes: {},
       order: {}
     }
+  },
+  componentDidMount: function() {
+    base.syncState(this.props.params.storeId + '/fishes', {
+      context: this,
+      state: 'fishes'
+    });
+
+    var localStorageRef = localStorage.getItem('order-'
+      + this.props.params.storeId);
+
+    if (localStorageRef) {
+      // update our component state to reflect what is in localStorage
+      this.setState({
+        order: JSON.parse(localStorageRef)
+      });
+    }
+  },
+  componentWillUpdate: function (nextProps, nextState) {
+    localStorage.setItem('order-' + this.props.params.storeId,
+      JSON.stringify(nextState.order));
   },
   addFish: function(fish) {
     var timestamp = (new Date()).getTime();
@@ -151,7 +177,7 @@ var Order = React.createClass({
     }
 
     return (
-      <li>
+      <li key={key}>
         {count}lbs
         {fish.name}
         <span className='price'>{h.formatPrice(count * fish.price)}</span>
