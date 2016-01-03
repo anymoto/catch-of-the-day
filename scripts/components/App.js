a60bc5a75ab90a79b8158584c120dce402907f00
anymoto <ana@castromartinez.com>
2016-01-03 17:02:00 -0600

    Working with ES6 and State

--- scripts/components/App.js
+++ scripts/components/App.js
@@ -8,6 +8,8 @@ import Fish from './Fish';
 import Order from './Order';
 import Inventory from './Inventory';
 import Catalyst from 'react-catalyst';
+import reactMixin from 'react-mixin';
+import autobind from 'autobind-decorator';
 
 /*
   Firebase
@@ -15,83 +17,97 @@ import Catalyst from 'react-catalyst';
 import Rebase from 're-base';
 var base = Rebase.createClass('https://catch-of-the-day-ana.firebaseio.com/');
 
-var App = React.createClass({
-  mixins: [Catalyst.LinkedStateMixin],
-  getInitialState: function() {
-    return {
-      fishes: {},
-      order: {}
+@autobind
+class App extends React.Component {
+
+    constructor() {
+      super();
+
+      this.state = {
+        fishes: {},
+        order: {}
+      }
     }
-  },
-  componentDidMount: function() {
-    base.syncState(this.props.params.storeId + '/fishes', {
-      context: this,
-      state: 'fishes'
-    });
 
-    var localStorageRef = localStorage.getItem('order-'
-      + this.props.params.storeId);
+    componentDidMount() {
+      base.syncState(this.props.params.storeId + '/fishes', {
+        context: this,
+        state: 'fishes'
+      });
 
-    if (localStorageRef) {
-      // update our component state to reflect what is in localStorage
+      var localStorageRef = localStorage.getItem('order-'
+        + this.props.params.storeId);
+
+      if (localStorageRef) {
+        // update our component state to reflect what is in localStorage
+        this.setState({
+          order: JSON.parse(localStorageRef)
+        });
+      }
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+      localStorage.setItem('order-' + this.props.params.storeId,
+        JSON.stringify(nextState.order));
+    }
+
+    addFish(fish) {
+      var timestamp = (new Date()).getTime();
+      // update the state object
+      this.state.fishes['fish-' + timestamp] = fish;
+      // set the state
+      this.setState({ fishes: this.state.fishes });
+    }
+
+    loadSamples() {
       this.setState({
-        order: JSON.parse(localStorageRef)
+        fishes: require('../sample-fishes')
       });
     }
-  },
-  componentWillUpdate: function (nextProps, nextState) {
-    localStorage.setItem('order-' + this.props.params.storeId,
-      JSON.stringify(nextState.order));
-  },
-  addFish: function(fish) {
-    var timestamp = (new Date()).getTime();
-    // update the state object
-    this.state.fishes['fish-' + timestamp] = fish;
-    // set the state
-    this.setState({ fishes: this.state.fishes });
-  },
-  loadSamples: function() {
-    this.setState({
-      fishes: require('../sample-fishes')
-    });
-  },
-  renderFish: function(key) {
-    return <Fish key={key} index={key} details={this.state.fishes[key]}
-    addToOrder={this.addToOrder}/>
-  },
-  removeFish: function(key) {
-    if (confirm('Are you sure you want to remove this fish?!')) {
-      this.state.fishes[key] = null;
-      this.setState({
-        fishes: this.state.fishes
-      });
+
+    renderFish(key) {
+      return <Fish key={key} index={key} details={this.state.fishes[key]}
+      addToOrder={this.addToOrder}/>
     }
-  },
-  addToOrder: function(key) {
-    this.state.order[key] = this.state.order[key] + 1 || 1;
-    this.setState({ order: this.state.order });
-  },
-  removeFromOrder: function(key) {
-    delete this.state.order[key];
-    this.setState({ order: this.state.order });
-  },
-  render: function() {
-    return (
-      <div className='catch-of-the-day'>
-        <div className='menu'>
-          <Header tagline='Fresh Seafood Good' />
-          <ul className='list-of-fishes'>
-            {Object.keys(this.state.fishes).map(this.renderFish)}
-          </ul>
+
+    removeFish(key) {
+      if (confirm('Are you sure you want to remove this fish?!')) {
+        this.state.fishes[key] = null;
+        this.setState({
+          fishes: this.state.fishes
+        });
+      }
+    }
+
+    addToOrder(key) {
+      this.state.order[key] = this.state.order[key] + 1 || 1;
+      this.setState({ order: this.state.order });
+    }
+
+    removeFromOrder(key) {
+      delete this.state.order[key];
+      this.setState({ order: this.state.order });
+    }
+
+    render() {
+      return (
+        <div className='catch-of-the-day'>
+          <div className='menu'>
+            <Header tagline='Fresh Seafood Good' />
+            <ul className='list-of-fishes'>
+              {Object.keys(this.state.fishes).map(this.renderFish)}
+            </ul>
+          </div>
+          <Order order={this.state.order} fishes={this.state.fishes} removeFromOrder={this.removeFromOrder}/>
+          <Inventory addFish={this.addFish} loadSamples={this.loadSamples}
+            fishes={this.state.fishes} linkState={this.linkState.bind(this)}
+            removeFish={this.removeFish}
+          />
         </div>
-        <Order order={this.state.order} fishes={this.state.fishes} removeFromOrder={this.removeFromOrder}/>
-        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}
-          fishes={this.state.fishes} linkState={this.linkState}
-          removeFish={this.removeFish}
-        />
-      </div>
-    )
-  }
-});
+      )
+    }
+}
+
+reactMixin.onClass(App, Catalyst.LinkedStateMixin);
 
 export default App;
